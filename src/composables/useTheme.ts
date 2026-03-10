7410ea931b08817065b25adafc1941de13f7a7dd
user <user@Kenny-MacBook-Pro.local>
2026-03-10 16:14:48 +0800

    save theme in local

--- src/composables/useTheme.ts
+++ src/composables/useTheme.ts
@@ -1,18 +1,31 @@
-import { ref } from 'vue'
-
 const theme = ref<'light' | 'dark'>('light')
 
 export function useTheme() {
+
+  const setTheme = (mode: 'light' | 'dark') => {
+    theme.value = mode
+    localStorage.setItem('theme', mode)
+
+    if (mode === 'dark')
+      document.documentElement.classList.add('dark')
+    else
+      document.documentElement.classList.remove('dark')
+  }
+
   const toggleTheme = () => {
-    theme.value = theme.value === 'light' ? 'dark' : 'light'
-    if (theme.value === 'dark') document.documentElement.classList.add('dark')
-    else document.documentElement.classList.remove('dark')
+    setTheme(theme.value === 'light' ? 'dark' : 'light')
   }
 
   // 初始化
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    theme.value = 'dark'
-    document.documentElement.classList.add('dark')
+  const savedTheme = localStorage.getItem('theme')
+
+  if (savedTheme === 'dark' || savedTheme === 'light') {
+    setTheme(savedTheme)
+  } else if (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  ) {
+    setTheme('dark')
   }
 
   return { theme, toggleTheme }
